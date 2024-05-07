0eadac759110e26433f186670adba60319dda6c7
Valtteri Erkkilä <valtteri.erkkila@gmail.com>
2024-05-08 00:35:19 +0300

    Preliminary support for tier tokens, T4-6 supported

--- AppearanceCollectionTooltip.lua
+++ AppearanceCollectionTooltip.lua
@@ -1,24 +1,55 @@
-local function fnAddAppearanceInfo(self)
-	local link = select(2, self:GetItem())
-	if not link then return end
-	
-	local itemQuality = select(3, GetItemInfo(link))
-	if itemQuality and itemQuality < 2 then return end --disregard poor and common items
-	
-	local id = string.match(link, "item:(%d*)")
-	if id then
-		local sourceID = select(2, C_TransmogCollection.GetItemInfo(id))
-		if not sourceID then return end
-		local isCollected = select(5, C_TransmogCollection.GetAppearanceSourceInfo(sourceID))
-		local canCollect = select(2, C_TransmogCollection.PlayerCanCollectSource(sourceID))
-		if isCollected then
-			self:AddDoubleLine("Appearance", "Collected", 1, 1, 1, 0, 1, 0)
+local addonName, ACT = ...
+
+ACT.COLLECTED = 1
+ACT.COLLECTABLE = 2
+ACT.NOT_COLLECTED = 3
+
+function ACT.GetAppearanceCollectionStatus(itemId)
+	if not itemId then return end
+	local sourceId = select(2, C_TransmogCollection.GetItemInfo(itemId))
+	if not sourceId then return end
+	local isCollected = select(5, C_TransmogCollection.GetAppearanceSourceInfo(sourceId))
+	local canCollect = select(2, C_TransmogCollection.PlayerCanCollectSource(sourceId))
+	if isCollected then
+		return ACT.COLLECTED
+	else
+		if canCollect then
+			return ACT.COLLECTABLE
 		else
-			if canCollect then
-				self:AddDoubleLine("Appearance", "Not collected", 1, 1, 1, 1, 1, 0)
-			else
-				self:AddDoubleLine("Appearance", "Not collected", 1, 1, 1, 1, 0, 0)
-			end
+			return ACT.NOT_COLLECTED
+		end
+	end
+end
+
+local function ACT_SetTooltip(tooltip, collectionStatus)
+	if collectionStatus == ACT.COLLECTED then
+		tooltip:AddDoubleLine("Appearance", "Collected", 1, 1, 1, 0, 1, 0)
+	else
+		if collectionStatus == ACT.COLLECTABLE then
+			tooltip:AddDoubleLine("Appearance", "Not collected", 1, 1, 1, 1, 1, 0)
+		else
+			tooltip:AddDoubleLine("Appearance", "Not collected", 1, 1, 1, 1, 0, 0)
+		end
+	end
+end
+
+local function fnAddAppearanceInfo(self)
+	local name, link = self:GetItem()
+	if not link then return end
+
+	if ACT.IsItemTierToken(name, link) then
+		local status = ACT.GetTierTokenStatus(name)
+		if status > 0 then
+			ACT_SetTooltip(self, status)
+		end
+	else
+		local itemQuality = select(3, GetItemInfo(link))
+		if itemQuality and itemQuality < 2 then return end --disregard poor and common items
+	
+		local id = string.match(link, "item:(%d*)")
+		if id then
+			local status = ACT.GetAppearanceCollectionStatus(id)
+			ACT_SetTooltip(self, status)
 		end
 	end
 end
---
+++ TierToken.lua
@@ -0,0 +1,127 @@
+local addonName, ACT = ...
+
+local helmTokens = {
+    "Helm",
+    "Crown",
+    "Regalia",
+    "Mark of Sanctification"
+}
+
+local shoulderTokens = {
+    "Pauldrons",
+    "Mantle",
+    "Regalia",
+    "Mark of Sanctification"
+}
+
+local chestTokens = {
+    "Chestguard",
+    "Breastplate",
+    "Regalia",
+    "Mark of Sanctification"
+}
+
+local legTokens = {
+    "Leggings",
+    "Legplates",
+    "Regalia",
+    "Mark of Sanctification"
+}
+
+local gloveTokens = {
+    "Gloves",
+    "Gauntlets",
+    "Regalia",
+    "Mark of Sanctification"
+}
+
+local tierInfo = {
+    ["Fallen"] = ACT.TIER_4,
+    ["Vanquished"] = ACT.TIER_5,
+    ["Forgotten"] = ACT.TIER_6,
+    ["Lost"] = ACT.TIER_7_10,
+    ["Wayward"] = ACT.TIER_8_10,
+    ["Grand"] = ACT.TIER_9
+}
+
+local classInfo = {
+    ["Champion"] = {"PALADIN", "ROGUE", "SHAMAN"},
+    ["Defender"] = {"WARRIOR", "PRIEST", "DRUID"},
+    ["Hero"] = {"HUNTER", "MAGE", "WARLOCK"},
+    ["Conqueror"] = {"PALADIN", "PRIEST", "WARLOCK"},
+    ["Protector"] = {"WARRIOR", "HUNTER", "SHAMAN"},
+    ["Vanquisher"] = {"ROGUE", "MAGE", "DRUID", "DEATHKNIGHT"},
+}
+
+local tokenAttr = {}
+
+function ACT.SlotInfoForToken(phrase)
+    for i=1, 4 do
+        if helmTokens[i] == phrase then return "HEADSLOT" end
+        if shoulderTokens[i] == phrase then return "SHOULDERSLOT" end
+        if chestTokens[i] == phrase then return "CHESTSLOT" end
+        if legTokens[i] == phrase then return "LEGSSLOT" end
+        if gloveTokens[i] == phrase then return "HANDSSLOT" end
+        if phrase == "Bracers" then return "WRISTSLOT" end
+        if phrase == "Belt" then return "WAISTSLOT" end
+        if phrase == "Boots" then return "FEETSLOT" end
+    end
+    return nil
+end
+
+function ACT.TierInfoForToken(phrase)
+    if phrase then return tierInfo[phrase] end
+    return nil
+end
+
+local function ACT_TokenizeTierTokenName(fullName)
+    local splitName = {}
+    for token in string.gmatch(fullName, "[^%s]+") do
+        if token ~= "of" and token ~= "the" then
+            table.insert(splitName, token)
+        end
+    end
+    return splitName
+end
+
+local function ACT_GetTokenAttributes(tokenizedStr)
+    local ret = {}
+    for _, t in pairs(tokenizedStr) do
+        local s, t, c = ACT.SlotInfoForToken(t), ACT.TierInfoForToken(t), classInfo[t]
+        if s then ret["slot"] = s end
+        if t then ret["tier"] = t end
+        if c then ret["classes"] = c end
+    end
+    return ret.slot, ret.tier, ret.classes
+end
+
+local function ACT_IsTokenForMyClass(classesList)
+    local myClass = select(2, UnitClass("player"))
+    for _, c in pairs(classesList) do
+        if myClass == c then return true end
+    end
+    return false
+end
+
+function ACT.IsItemTierToken(itemName, itemLink)
+    local classId, subclassId = select(12, GetItemInfo(itemLink))
+    if classId ~= 15 or subclassId ~= 0 then return false end -- tier tokens are officially miscellaneous junk
+    local splitName = ACT_TokenizeTierTokenName(itemName)
+    local slot, tier, classes = ACT_GetTokenAttributes(splitName)
+    return slot and tier and classes
+end
+
+--Slot of the Tier Classes
+function ACT.GetTierTokenStatus(tokenName)
+    local splitName = ACT_TokenizeTierTokenName(tokenName)
+    local slot, tier, classes = ACT_GetTokenAttributes(splitName)
+    local myClassName = select(2, UnitClass("player"))
+    for _, c in pairs(classes) do
+        if c == myClassName then
+            local id = ACT.TierLookup[tier][myClassName][slot]
+            if not id or id == 0 then return 0 end
+            return ACT.GetAppearanceCollectionStatus(id)
+        end
+    end
+    return 0
+end
